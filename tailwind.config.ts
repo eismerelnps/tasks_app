80bd27b7aab2d675eb8ee435546e000237a78c60
Eismer Lobaina <eismerlobaina@gmail.com>
2024-03-02 11:58:58 -0500

    mod: set up tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,55 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,ts,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'chat_background_dark': "url('/public/chat_background/chat_background_dark.png')",
+        'chat_background_light': "url('/public/chat_background/chat_background_light.png')",
+      },
+      colors: {
+        gold: '#ffd700', // Color para la medalla de oro
+        silver: '#c0c0c0', // Color para la medalla de plata
+        bronze: '#cd7f32', // Color para la medalla de bronce
+        primary: {
+          '50': '#EAF0F5',
+          '100': '#8A94A6',
+          '200': '#c6cdd2',
+          '300': '#8A94A6',
+          '400': '#',
+          '500': '#',
+          '600': '#7c8694',
+          '700': '#6f7886',
+          '800': '#5e646f',
+          '900': '#4d535b',
+          '950': '#323439',
+        },
+        cerulean_blue: {
+          '50': '#edf8ff',
+          '100': '#d7eeff',
+          '200': '#b7e4ff',
+          '300': '#86d5ff',
+          '400': '#4dbbff',
+          '500': '#249aff',
+          '600': '#0d7bff',
+          '700': '#0663ef',
+          '800': '#0d55cf',
+          '900': '#114697',
+          '950': '#0f2c5c',
+        },
+
+
       },
     },
   },
+  variants: {},
   plugins: [],
 };
-export default config;
